97f1628b8a85e981cf6cb1e43d8d9782c10a9d60
Ramón Huidobro <hola@ramonh.dev>
2020-06-28 12:56:38 +0200

    Turn it into an electron app

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-const path = require("path")
-const Server = require("http").Server;
-const express = require("express");
-const app = express();
-const server = new Server(app);
-const opn = require('opn');
+const url = require('url')
+const path = require('path')
 const robot = require("robotjs");
+const { app, BrowserWindow, ipcMain, systemPreferences } = require('electron')
 const os = require('os');
 
-app.use(express.static(path.join(__dirname, "app")));
-app.post("/", function (req, res) {
+async function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 400,
+    height:160,
+    transparent: true,
+    hasShadow: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  var micAccess = false
+  try {
+    if (os.platform() !== "darwin") {
+      micAccess = true;
+    }
+
+    const status = await systemPreferences.getMediaAccessStatus("microphone");
+    console.log("Current microphone access status:", status);
+
+    if (status === "not-determined") {
+      const success = await systemPreferences.askForMediaAccess("microphone");
+      log.info("Result of microphone access:", success.valueOf() ? "granted" : "denied");
+      micAccess = success.valueOf();
+    }
+
+    micAccess = status === "granted";
+  } catch (error) {
+    console.log("Could not get microphone permission:", error.message);
+  }
+  if (micAccess) {
+    // and load the index.html of the app.
+    win.setAutoHideMenuBar(true);
+    win.setAlwaysOnTop(true, 'floating');
+    win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'app/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
+}
+
+ipcMain.on('clap', (event, arg) => {
   console.log("👏");
   if (os.platform() === 'win32') {
-	  robot.typeString(":clap:");
+    robot.typeString(":clap:");
   }
-	else {
-	  robot.typeString("👏");
+  else {
+    robot.typeString("👏");
   }
   robot.keyTap('enter')
-  res.send("POST request!!!");
 });
 
-
-const port = process.env.PORT || 8080;
-const listen = new Promise(resolve => {
-  server.listen(port, resolve);
-});
-
-listen.then(() => {
-  console.log(`http://localhost:${port}`);
-  opn(`http://localhost:${port}`);
-  process.on("SIGINT", () => {
-    server.close();
-    process.exit();
-  });
-});
+app.whenReady().then(createWindow)
